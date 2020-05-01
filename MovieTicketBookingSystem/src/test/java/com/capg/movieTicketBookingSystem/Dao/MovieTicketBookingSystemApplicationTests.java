a416020980d12c72ad7dbb47136c937a0dc8007e
WizardX7 <60417591+WizardX7@users.noreply.github.com>
2020-05-01 19:34:22 +0530

    .

---
+++ MovieTicketBookingSystem/src/test/java/com/capg/movieTicketBookingSystem/Dao/MovieTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.movieTicketBookingSystem.Dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
