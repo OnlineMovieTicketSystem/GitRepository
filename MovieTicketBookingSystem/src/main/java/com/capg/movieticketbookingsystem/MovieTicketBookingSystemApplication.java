414b1ede82ecc145b339057ea259dc46d938a417
ShivaniTandon <shivanitandon26@gmail.com>
2020-05-02 10:13:03 +0530

    Commit

--- MovieTicketBookingSystem/src/main/java/com/capg/movieticketbookingsystem/MovieTicketBookingSystemApplication.java
+++ MovieTicketBookingSystem/src/main/java/com/capg/movieticketbookingsystem/MovieTicketBookingSystemApplication.java
@@ -1,4 +1,4 @@
-package com.capg.movieTicketBookingSystem.Dao;
+package com.capg.movieTicketBookingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
