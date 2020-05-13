9f70fcfd24c0e643a5142b13b121761ad1f27bc4
ShivaniTandon <60996987+ShivaniTandon@users.noreply.github.com>
2020-05-13 10:28:54 +0530

    Update AdminService.java

--- Movie-Ticket-Booking-System/src/main/java/com/mtbs/service/AdminService.java
+++ Movie-Ticket-Booking-System/src/main/java/com/mtbs/service/AdminService.java
@@ -1,7 +1,8 @@
 package com.mtbs.service;
 
+import com.mtbs.exception.AdminNotFoundException;
 import com.mtbs.model.Admin;
 
 public interface AdminService {
-	public Admin getAdmin(int id, String password);
+	public Admin getAdmin(int id, String password) throws AdminNotFoundException;
 }
