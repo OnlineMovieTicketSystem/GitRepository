9f8c17328c177be4b51b228c370c4b4350b3884b
ShivaniTandon <60996987+ShivaniTandon@users.noreply.github.com>
2020-05-13 10:48:08 +0530

    Delete AdminService.java

--- Movie-Ticket-Booking-System/src/main/java/com/mtbs/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.mtbs.service;
-
-import com.mtbs.exception.AdminNotFoundException;
-import com.mtbs.model.Admin;
-
-public interface AdminService {
-	public Admin getAdmin(int id, String password) throws AdminNotFoundException;
-}
